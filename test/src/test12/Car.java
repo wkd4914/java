4a0098ea8d46dba136e1f77f82bda948ed203f0c
koitt03-A <koitt03-A@koitt03-A-PC>
2018-07-20 17:51:44 +0900

    bffv

--- test/src/test12/Car.java
+++ test/src/test12/Car.java
@@ -11,11 +11,11 @@ public class Car {
 //  접근 제어자 (static)데이터타입(리턴값이 있을시)  메소드의 이름(){
 //	           (리턴값이 없을때 void)
 
-	public void make(String company) {
+	public void make(String company) {//private -> public 으로 변환 하면서  
 		this.company = company;
 	}
 	
-	public String give() {
+	public String give() {// private이므로 car클래스에서 불러 올수 없으므로 리턴값으로 호출을 한다.
 		return company;
 	}
 	
